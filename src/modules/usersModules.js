527be8d9e49aa554524d8f1af869dfb418703325
rakaso598 <nexxus86@outlook.com>
2025-03-17 11:46:32 +0900

    10

--- src/modules/usersModules.js
+++ src/modules/usersModules.js
@@ -1,4 +1,5 @@
 import express from "express";
+import prisma from "../db/prisma/clientPrisma.js";
 
 const usersRouter = express.Router();
 
@@ -6,10 +7,19 @@ usersRouter.post("/sign-up", async (req, res, next) => {
   try {
     const data = req.body;
     console.log(data);
-
     // DB에 회원정보를 넣어서 저장할 차례 -> ORM 프리즈마를 사용하려면
 
-    res.send("OK");
+    // DB랑 통신하는 코드는 -> 비동기적으로 작동
+    const user = await prisma.user.create({
+      data: {
+        email: data.email,
+        name: data.name,
+        encryptedPassword: "$" + data.password + "@",
+      },
+    });
+    const json = JSON.stringify(user);
+
+    res.send(json);
   } catch (e) {
     next(e);
   }
