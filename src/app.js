fd7260a7e2a79958f0ffaa412ae947da6e50e1f0
rakaso598 <nexxus86@outlook.com>
2025-03-17 11:29:33 +0900

    5

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ import express from "express";
 const app = express();
 const PORT = 5555;
 
-app.get("/health-check", (req, res, next) => {
-  res.status(200).send("OK");
-});
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server started to listen at ${PORT}`);
---
+++ src/modules/indexModule.js
@@ -0,0 +1,12 @@
+import express from "express";
+import usersRouter from "./usersModules";
+
+const router = express.Router();
+
+router.use("/users", usersRouter);
+
+router.get("/health-check", (req, res, next) => {
+  res.status(200).send("OK");
+});
+
+export default router;
