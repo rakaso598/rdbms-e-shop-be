709b639ce883fa8107cb595310a410409bedbbde
rakaso598 <nexxus86@outlook.com>
2025-03-17 11:37:14 +0900

    8

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import router from "./modules/indexModule.js";
 const app = express();
 const PORT = 5555;
 
+app.use(express.json());
+
 app.use(router);
 
 app.listen(PORT, () => {
--- src/modules/usersModules.js
+++ src/modules/usersModules.js
@@ -7,6 +7,8 @@ usersRouter.post("/sign-up", async (req, res, next) => {
     const data = req.body;
     console.log(data);
 
+    // DB에 회원정보를 넣어서 저장할 차례 -> ORM 프리즈마를 사용하려면
+
     res.send("OK");
   } catch (e) {
     next(e);
